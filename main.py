ff043faeec6eed37b21341ca41ba8e37de7b3645
Lowlights <125327040+Lowlightsas@users.noreply.github.com>
2023-07-20 18:56:07 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hello')
+from bs4 import BeautifulSoup
+import requests
+url =  "https://www.film.ru/compilation/100-luchshih-filmov-xxi-veka-po-versii-zhurnala-empire"
+req = requests.get(url)
+src = req.text
+soap = BeautifulSoup(src,'lxml')
+
+s = soap.find_all('a',class_="film_list_link")
+
+for k in s:
+    name = k.text.strip().split()
+    hrf =  'https://www.film.ru' + k.get('href')
+    print(name,hrf)
+
